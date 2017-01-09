2000022fbf6e7d21d0ddb661691d8ebc7725599d
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-09 13:50:50 -0500

    se atiende solicitudes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-var express = require("express");
-var app = express();
-app.listen(8080);
+var express = require("express");// inportamos el paquete
+var app = express();// inicialixamos el objeto express
+
+app.get("/",function(req,res) {
+  res.send("hola mundo con express!!");// invia la respuesta y cierra la conexion
+});
+
+app.listen(8080);// escuchamos peticiones
